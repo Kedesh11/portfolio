5677ed5b454ff80381039e353fc94ce4bdb6873e
IKISSA PENDY Sevan Kedesh <pendysevan10@gmail.com>
2023-10-04 11:52:38 +0100

    responsive done

--- src/App.js
+++ src/App.js
@@ -14,12 +14,8 @@ export default function App() {
         <div class="hidden space-x-8 lg:flex">
           <ul className="flex gap-10">
             <li><a href="#">Home</a></li>
-            <li><a href="#">Features</a></li>
             <li><a href="#">Portfolio</a></li>
             <li><a href="#">Resume</a></li>
-            <li><a href="#">Testimonial</a></li>
-            <li><a href="#">Clients</a></li>
-            <li><a href="#">Pricing</a></li>
             <li><a href="#">Blog</a></li>
             <li><a href="#">Contatcs</a></li>
           </ul>
@@ -44,13 +40,21 @@ export default function App() {
       </nav>
 
       {/* Section of biography */}
-      <div></div>
+      <div className="my-40">
+        <div className="lg:flex lg:justify-between">
+          <div className="w-[360px] h-full rounded-[10px] bg-[#17181b] biography my-[1em] flex justify-center ">
+            <div className="w-full h-[400px]  relative">
+              <img src="/profile-removebg.png" className="width-[350] h-[500px] absolute bottom-1 z-10" />
+            </div>
+          </div>
+        </div>
+      </div>
 
       {/* WHAT I DO */}
       <div className="w-[90%] h-[1px] bg-[#121415] my-[3em] mx-auto"></div>
       <div>
         <h3 className="text-[#E2024E] flex justify-center">FEATURES</h3>
-        <h1 className="text-[#878e99] text-6xl flex justify-center font-bold">WHAT I DO</h1>
+        <h1 className="text-[#878e99] text-2xl lg:text-6xl flex justify-center font-bold">WHAT I DO</h1>
         <div className="grid gap-10 my-[3em] sm:grid-cols md:grid-cols-2 lg:grid-cols-3">
           <a href="#">
             <div className="h-[350px] w-[350px] rounded-lg flex flex-col px-[2em] blog-card"></div>
@@ -82,13 +86,13 @@ export default function App() {
       <div className="w-[90%] h-[1px] bg-[#121415] my-[3em] mx-auto"></div>
       <div>
         <h3 className="text-[#E2024E] flex justify-center my-[1em]">VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK</h3>
-        <h1 className="text-[#c4cfde] text-6xl flex justify-center font-bold">My Portfolio</h1>
+        <h1 className="text-[#c4cfde] text-2xl lg:text-6xl flex justify-center font-bold">My Portfolio</h1>
         <div className="grid gap-10 sm:grid-cols md:grid-cols-2 lg:grid-cols-3 lg:justify-between my-[3em] ">
           <a href="#">
             <div className="h-[500px] w-[400px] rounded-lg flex flex-col px-[2em] blog-card">
               <div className="h-[60%] flex items-center justify-center">
                 <div className="h-[80%] w-full bg-white rounded-lg">
-                    <img src = "/portfolio1.jpg" className="rounded-lg"/>
+                  <img src="/portfolio1.jpg" className="rounded-lg" />
                 </div>
               </div>
               <div className="h-[40%]">
@@ -109,7 +113,7 @@ export default function App() {
             <div className="h-[500px] w-[400px] rounded-lg flex flex-col px-[2em] blog-card">
               <div className="h-[60%] flex items-center justify-center">
                 <div className="h-[80%] w-full bg-white rounded-lg">
-                  <img src = "/portfolio2.jpg" className="rounded-lg"/>
+                  <img src="/portfolio2.jpg" className="rounded-lg" />
                 </div>
               </div>
               <div className="h-[40%]">
@@ -130,7 +134,7 @@ export default function App() {
             <div className="h-[500px] w-[400px] rounded-lg flex flex-col px-[2em] blog-card">
               <div className="h-[60%] flex items-center justify-center">
                 <div className="h-[80%] w-full bg-white rounded-lg">
-                  <img src = "/portfolio3.jpg" className="rounded-lg"/>
+                  <img src="/portfolio3.jpg" className="rounded-lg" />
                 </div>
               </div>
               <div className="h-[40%]">
@@ -151,7 +155,7 @@ export default function App() {
             <div className="h-[500px] w-[400px] rounded-lg flex flex-col px-[2em] blog-card">
               <div className="h-[60%] flex items-center justify-center">
                 <div className="h-[80%] w-full bg-white rounded-lg">
-                  <img src = "/portfolio4.jpg" className="rounded-lg"/>
+                  <img src="/portfolio4.jpg" className="rounded-lg" />
                 </div>
               </div>
               <div className="h-[40%]">
@@ -172,7 +176,7 @@ export default function App() {
             <div className="h-[500px] w-[400px] rounded-lg flex flex-col px-[2em] blog-card">
               <div className="h-[60%] flex items-center justify-center">
                 <div className="h-[80%] w-full bg-white rounded-lg">
-                  <img src = "/portfolio5.jpg" className="rounded-lg"/>
+                  <img src="/portfolio5.jpg" className="rounded-lg" />
                 </div>
               </div>
               <div className="h-[40%]">
@@ -193,7 +197,7 @@ export default function App() {
             <div className="h-[500px] w-[400px] rounded-lg flex flex-col px-[2em] blog-card">
               <div className="h-[60%] flex items-center justify-center">
                 <div className="h-[80%] w-full bg-white rounded-lg">
-                  <img src = "/portfolio6.jpg" className="rounded-lg"/>
+                  <img src="/portfolio6.jpg" className="rounded-lg" />
                 </div>
               </div>
               <div className="h-[40%]">
@@ -216,8 +220,21 @@ export default function App() {
 
       <div className="w-[90%] h-[1px] bg-[#121415] my-[3em] mx-auto"></div>
       <div>
-        <h1 className="text-6xl font-bold text-[#878e99] flex justify-center">My Resume</h1>
-        <div className="flex flex-col gap-5 lg:flex justify-between my-[2em]">
+        <h1 className=" text-2xl lg:text-6xl font-bold text-[#878e99] flex justify-center">My Resume</h1>
+
+        {/* Resume in desktop */}
+        <div className="hidden lg:flex gap-5 my-[2em] lg:justify-between ">
+          <Button className="text-[#c4cfde] hover:text-[#E2024E] py-[2em] px-[7em] rounded-lg resume" title="Education" />
+
+          <Button className="text-[#c4cfde] hover:text-[#E2024E] py-[2em] px-[7em] rounded-lg resume" title="Professional Skills" />
+
+          <Button className="text-[#c4cfde] hover:text-[#E2024E] py-[2em] px-[7em] rounded-lg resume" title="Experience" />
+
+          <Button className="text-[#c4cfde] hover:text-[#E2024E] py-[2em] px-[7em] rounded-lg resume" title="Interview" />
+        </div>
+
+        {/* Resume in phone and matlob */}
+        <div className="flex flex-col gap-5 my-[2em] lg:hidden">
           <Button className="text-[#c4cfde] hover:text-[#E2024E] py-[2em] px-[7em] rounded-lg resume" title="Education" />
 
           <Button className="text-[#c4cfde] hover:text-[#E2024E] py-[2em] px-[7em] rounded-lg resume" title="Professional Skills" />
@@ -318,7 +335,7 @@ export default function App() {
       <div>
         <div className="flex flex-col justify-center items-center gap-5 my-[3em]">
           <h3 className="text-xl text-[#E2024E]">VISIT MY BLOG  AND KEEP  YOUR FEEDBACK</h3>
-          <h1 className="text-6xl text-[#c4cfde] font-bold">My Blog</h1>
+          <h1 className="text-2xl lg:text-6xl text-[#c4cfde] font-bold">My Blog</h1>
         </div>
         {/* Blog card */}
 
@@ -327,7 +344,7 @@ export default function App() {
             <div className="h-[500px] w-[400px] rounded-lg flex flex-col px-[2em] blog-card">
               <div className="h-[60%] flex items-center justify-center">
                 <div className="h-[80%] w-full bg-white rounded-lg">
-                  <img src = "/blog1.jpg" className="rounded-lg"/>
+                  <img src="/blog1.jpg" className="rounded-lg" />
                 </div>
               </div>
               <div className="h-[40%]">
@@ -347,7 +364,7 @@ export default function App() {
             <div className="h-[500px] w-[400px] rounded-lg flex flex-col px-[2em] blog-card">
               <div className="h-[60%] flex items-center justify-center">
                 <div className="h-[80%] w-full bg-white rounded-lg">
-                  <img src = "/blog2.jpg" className="rounded-lg"/>
+                  <img src="/blog2.jpg" className="rounded-lg" />
                 </div>
               </div>
               <div className="h-[40%]">
@@ -367,7 +384,7 @@ export default function App() {
             <div className="h-[500px] w-[400px] rounded-lg flex flex-col px-[2em] blog-card">
               <div className="h-[60%] flex items-center justify-center">
                 <div className="h-[80%] w-full bg-white rounded-lg">
-                  <img src = "/blog3.jpg" className="rounded-lg"/>
+                  <img src="/blog3.jpg" className="rounded-lg" />
                 </div>
               </div>
               <div className="h-[40%]">
@@ -387,7 +404,7 @@ export default function App() {
             <div className="h-[500px] w-[400px] rounded-lg flex flex-col px-[2em] blog-card">
               <div className="h-[60%] flex items-center justify-center">
                 <div className="h-[80%] w-full bg-white rounded-lg">
-                  <img src = "/blog3.jpg" className="rounded-lg"/>
+                  <img src="/blog3.jpg" className="rounded-lg" />
                 </div>
               </div>
               <div className="h-[40%]">
@@ -411,60 +428,115 @@ export default function App() {
       <div>
         <div className="flex flex-col justify-center items-center my-[3em]">
           <h4 className="text-[#E2024E] text-xl my-[1em]">CONTACT</h4>
-          <h1 className="text-6xl text-[#c4cfde] font-bold ">Contact With Me</h1>
+          <h1 className="text-2xl lg:text-6xl text-[#c4cfde] font-bold ">Contact With Me</h1>
         </div>
 
-        <div className="flex justify-between">
-          {/* Section left  */}
-          <div className="px-[3em] w-[30%] h-[700px] rounded-lg flex flex-col justify-between py-[4em] div-left">
-            <div className="w-[200px] h-[200px] mx-auto bg-white rounded-lg rounded-full">
-              <img src="./profile.jpg" className="rounded-full h-full w-full" />
+        <div>
+          {/* responsive of desktop */}
+          <div className="hidden lg:flex lg:justify-between">
+            {/* Section left  */}
+            <div className="px-[3em] w-[30%] h-[700px] rounded-lg flex flex-col justify-between py-[4em] div-left">
+              <div className="w-[200px] h-[200px] mx-auto bg-white rounded-lg rounded-full">
+                <img src="./profile.jpg" className="rounded-full h-full w-full" />
+              </div>
+              <div className=" my-[1em]">
+                <h1 className="text-xl text-white my-[1em]">Marly MOUTSINGA</h1>
+                <h2 className="my-[1em]">Full Stack Developper</h2>
+                <p>Lorem ipsum dolor sit amet consectetur<br /> sed do eiusmod tempor elit</p>
+                <p>Phone: 213412434</p>
+                <p>Email: manprobleme@gmail.com</p>
+              </div>
+              <div>
+                <h1 className="text-[#878e99]">FIND WITH ME</h1>
+                <div className="flex justify-between"></div>
+              </div>
             </div>
-            <div className=" my-[1em]">
-              <h1 className="text-xl text-white my-[1em]">Marly MOUTSINGA</h1>
-              <h2 className="my-[1em]">Full Stack Developper</h2>
-              <p>Lorem ipsum dolor sit amet consectetur<br /> sed do eiusmod tempor elit</p>
-              <p>Phone: 213412434</p>
-              <p>Email: manprobleme@gmail.com</p>
-            </div>
-            <div>
-              <h1 className="text-[#878e99]">FIND WITH ME</h1>
-              <div className="flex justify-between"></div>
+
+            {/* Section right */}
+            <div className="w-[60%] rounded-lg p-[2em] div-form">
+              <form className="flex flex-col justify-between gap-10">
+                <div className="flex justify-between">
+                  <div className="flex flex-col w-[45%]">
+                    <label htmlFor="name" className="">YOUR NAME</label>
+                    <Input type="text" className="bg-[#191B1E] w-full h-[3em] rounded-lg" required />
+                  </div>
+                  <div className="flex flex-col w-[45%]">
+                    <label htmlFor="number">PHONE NUMBER</label>
+                    <Input type="text" className="bg-[#191B1E] w-full h-[3em] rounded-lg" required />
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <label tmlFor="email">EMAIL</label>
+                  <Input type="email" name="email" className="bg-[#191B1E] w-full h-[3em] rounded-lg" required />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="subject">SUBJECT</label>
+                  <Input type="text" name="subject" className="bg-[#191B1E] h-[4em] rounded-lg " required />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="subject">YOUR MESSAGE</label>
+                  <textarea name="message" className="h-[6em] bg-[#1F2226] rounded-lg"></textarea>
+                </div>
+                <button className="bg-[#1F2226] w-full text-white h-[4em] rounded-lg btn-form">SEND A MESSAGE</button>
+              </form>
             </div>
           </div>
-
-          {/* Section right */}
-          <div className="w-[60%] rounded-lg p-[2em] div-form">
-            <form className="flex flex-col justify-between gap-10">
-              <div className="flex justify-between">
-                <div className="flex flex-col w-[45%]">
-                  <label htmlFor="name" className="">YOUR NAME</label>
-                  <Input type="text" className="bg-[#191B1E] w-full h-[3em] rounded-lg" required />
-                </div>
-                <div className="flex flex-col w-[45%]">
-                  <label htmlFor="number">PHONE NUMBER</label>
-                  <Input type="text" className="bg-[#191B1E] w-full h-[3em] rounded-lg" required />
-                </div>
-              </div>
-              <div className="flex flex-col">
-                <label tmlFor="email">EMAIL</label>
-                <Input type="email" name="email" className="bg-[#191B1E] w-full h-[3em] rounded-lg" required />
-              </div>
-
-              <div className="flex flex-col">
-                <label htmlFor="subject">SUBJECT</label>
-                <Input type="text" name="subject" className="bg-[#191B1E] h-[4em] rounded-lg " required />
-              </div>
-
-              <div className="flex flex-col">
-                <label htmlFor="subject">YOUR MESSAGE</label>
-                <textarea name="message" className="h-[6em] bg-[#1F2226] rounded-lg"></textarea>
-              </div>
-              <button className="bg-[#1F2226] w-full text-white h-[4em] rounded-lg btn-form">SEND A MESSAGE</button>
-            </form>
-          </div>
-
         </div>
+
+        {/* Responsive of mobil and tablette */}
+        <div>
+          <div className="lg:hidden">
+            {/* Section left  */}
+            <div className="px-[3em] w-full h-full rounded-lg flex flex-col justify-between my-[2em] div-left">
+              <div className="w-[95%] h-[400px] mx-auto bg-white rounded-lg rounded-lg">
+                <img src="./profile.jpg" className="rounded-lg h-full w-full" />
+              </div>
+              <div className=" my-[1em]">
+                <h1 className="text-xl text-white">Marly MOUTSINGA</h1>
+                <h2 className="my-[1em]">Full Stack Developper</h2>
+                <p>Lorem ipsum dolor sit amet consectetur<br /> sed do eiusmod tempor elit</p>
+                <p>Phone: 213412434</p>
+                <p>Email: manprobleme@gmail.com</p>
+              </div>
+              <div>
+                <h1 className="text-[#878e99]">FIND WITH ME</h1>
+                <div className="flex justify-between"></div>
+              </div>
+            </div>
+
+            {/* Section right */}
+            <div className="w-full rounded-lg p-[2em] div-form">
+              <form className="flex flex-col justify-between gap-10">
+                  <div className="flex flex-col w-full">
+                    <label htmlFor="name" className="">YOUR NAME</label>
+                    <Input type="text" className="bg-[#191B1E] w-full h-[3em] rounded-lg" required />
+                  </div>
+                  <div className="flex flex-col w-full">
+                    <label htmlFor="number">PHONE NUMBER</label>
+                    <Input type="text" className="bg-[#191B1E] w-full h-[3em] rounded-lg" required />
+                  </div>
+                <div className="flex flex-col">
+                  <label tmlFor="email">EMAIL</label>
+                  <Input type="email" name="email" className="bg-[#191B1E] w-full h-[3em] rounded-lg" required />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="subject">SUBJECT</label>
+                  <Input type="text" name="subject" className="bg-[#191B1E] h-[4em] rounded-lg " required />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="subject">YOUR MESSAGE</label>
+                  <textarea name="message" className="h-[6em] bg-[#1F2226] rounded-lg"></textarea>
+                </div>
+                <button className="bg-[#1F2226] w-full text-white h-[4em] rounded-lg btn-form">SEND A MESSAGE</button>
+              </form>
+            </div>
+          </div>
+        </div>
+
       </div>
 
 
@@ -472,7 +544,7 @@ export default function App() {
       <div className="w-[90%] h-[1px] bg-[#121415] my-[2em] mx-auto"></div>
       <div className="flex justify-between">
         <div>
-          
+
         </div>
 
         <div>
