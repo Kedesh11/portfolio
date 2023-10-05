419c876a3de6e8dd8c2ddb70557a22b904f0dc28
IKISSA PENDY Sevan Kedesh <pendysevan10@gmail.com>
2023-10-05 05:06:47 +0100

    init

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+import profile from "./images/profile.jpg";
+import logo from "./images/logo.png";
+import profile_remove from "./images/profile_remove.png";
+import projectOne from "./images/projects/projectOne.jpg";
+import projectTwo from "./images/projects/projectTwo.jpg";
+import projectThree from "./images/projects/projectThree.jpeg";
+import quote from "./images/testmonial/quote.png";
+import contactImg from "./images/contact/contactImg.png";
+
+export {
+  logo,
+  profile,
+  profile_remove,
+  projectOne,
+  projectTwo,
+  projectThree,
+  quote,
+  contactImg,
+};
---
+++ src/data/data.js
@@ -0,0 +1,42 @@
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe} from "react-icons/fa";
+import { SiProgress, SiAntdesign } from "react-icons/si";
+
+// Features Data
+export const featuresData = [
+  {
+    id: 1,
+    title: "Creation des maquettes",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 2,
+    icon: <AiFillAppstore />,
+    title: "Developpement d'application",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 3,
+    icon: <SiProgress />,
+    title: "Consultant informatique",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 4,
+    icon: <FaMobile />,
+    title: "Developpement Mobile",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 5,
+    icon: <SiAntdesign />,
+    title: "UX Design",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 6,
+    icon: <FaGlobe />,
+    title: "Hebergeur de site",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+];
